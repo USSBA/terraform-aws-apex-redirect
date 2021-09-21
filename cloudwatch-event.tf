410197859e09823a94859e7db13c14ef2e7bfc2e
Tyler Webb <twebb@sba.gov>
2021-09-21 11:33:14 -0400

    Added count

--- cloudwatch-event.tf
+++ cloudwatch-event.tf
@@ -1,31 +1,32 @@
 data "archive_file" "apex_restart" {
+  count       = var.monthly_restart_enabled ? 1 : 0
   type        = "zip"
   source_dir  = "../../functions/"
   output_path = "../../functions/apex_restart.zip"
 }
 
 resource "aws_lambda_function" "apex_restart" {
+  count         = var.monthly_restart_enabled ? 1 : 0
   filename      = "../../functions/apex_restart.zip"
   function_name = var.service_name
-  role          = aws_iam_role.apex_restart.arn
-  handler       = "deploy.lambda_handler"
+  role          = aws_iam_role.apex_restart[count.index].arn
+  handler       = "apex-restart.lambda_handler"
   runtime       = "python3.9"
   timeout       = 10
 
-  source_code_hash = data.archive_file.apex_restart.output_base64sha256
+  source_code_hash = data.archive_file.apex_restart[count.index].output_base64sha256
 
   environment {
     variables = {
       CLUSTER        = "${var.cluster_name}"
       SERVICE        = "${var.service_name}"
-      SECURITY_GROUP = "${aws_security_group.apex.id}"
-      SUBNET_ONE     = "${var.subnet_ids[0]}"
-      SUBNET_TWO     = "${var.subnet_ids[1]}"
+      DESIRED_COUNT  = length(var.subnet_ids)
     }
   }
 }
 
 resource "aws_iam_role" "apex_restart" {
+  count = var.monthly_restart_enabled ? 1 : 0
   name = var.service_name
 
   assume_role_policy = <<EOF
@@ -45,6 +46,7 @@ EOF
 }
 
 data "aws_iam_policy_document" "apex_restart" {
+  count = var.monthly_restart_enabled ? 1 : 0
   statement {
     sid = "1"
 
@@ -83,33 +85,38 @@ data "aws_iam_policy_document" "apex_restart" {
 }
 
 resource "aws_iam_policy" "apex_restart" {
+  count  = var.monthly_restart_enabled ? 1 : 0
   name   = var.service_name
   path   = "/"
-  policy = data.aws_iam_policy_document.apex_restart.json
+  policy = data.aws_iam_policy_document.apex_restart[count.index].json
 }
 
 resource "aws_iam_role_policy_attachment" "apex_restart" {
-  role       = aws_iam_role.apex_restart.name
-  policy_arn = aws_iam_policy.apex_restart.arn
+  count      = var.monthly_restart_enabled ? 1 : 0
+  role       = aws_iam_role.apex_restart[count.index].name
+  policy_arn = aws_iam_policy.apex_restart[count.index].arn
 }
 
 resource "aws_cloudwatch_event_rule" "apex_restart" {
+  count               = var.monthly_restart_enabled ? 1 : 0
   name                = var.service_name
   description         = "triggers lambda that forces a new deployment of ${var.service_name}"
-  schedule_expression = "cron(0 5 1 * ? *)"#"rate(30 minutes)"
+  schedule_expression = "cron(0 5 1 * ? *)"
   is_enabled          = true
 }
 
 resource "aws_cloudwatch_event_target" "apex_restart" {
-  rule      = aws_cloudwatch_event_rule.apex_restart.name
-  target_id = aws_lambda_function.apex_restart.id
-  arn       = aws_lambda_function.apex_restart.arn
+  count     = var.monthly_restart_enabled ? 1 : 0
+  rule      = aws_cloudwatch_event_rule.apex_restart[count.index].name
+  target_id = aws_lambda_function.apex_restart[count.index].id
+  arn       = aws_lambda_function.apex_restart[count.index].arn
 }
 
 resource "aws_lambda_permission" "apex_restart" {
+  count         = var.monthly_restart_enabled ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.apex_restart.function_name
+  function_name = aws_lambda_function.apex_restart[count.index].function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.apex_restart.arn
+  source_arn    = aws_cloudwatch_event_rule.apex_restart[count.index].arn
 }
