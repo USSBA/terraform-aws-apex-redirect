c61fa0ac0356be05374138e69ec68075c1aaf6d6
Tyler Webb <twebb@sba.gov>
2021-09-21 11:31:45 -0400

    Removed non required attributes to update_service

--- functions/apex-restart.py
+++ functions/apex-restart.py
@@ -6,28 +6,12 @@ def lambda_handler(event, context):
     client = boto3.client('ecs')
     cluster=os.environ.get('CLUSTER')
     service=os.environ.get('SERVICE')
-    security_group=os.environ.get('SECURITY_GROUP')
-    subnet_one=os.environ.get('SUBNET_ONE')
-    subnet_two=os.environ.get('SUBNET_TWO')
+    desired_count=os.environ.get('DESIRED_COUNT')
 
     print("replacing apex service...")
     response = client.update_service(
         cluster=cluster,
         service=service,
-        desiredCount=2,
-        taskDefinition=service,
-        networkConfiguration={
-            'awsvpcConfiguration': {
-                'subnets': [
-                    subnet_one,
-                    subne_two
-                ]
-                'securityGroups': [
-                    security_group
-                ],
-                'assignPublicIp': 'ENABLED'
-            }
-        },
+        desiredCount=int(desired_count),
         forceNewDeployment=True,
-        enableExecuteCommand=True
     )
